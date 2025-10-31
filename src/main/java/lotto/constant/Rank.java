71a15a03b013c28ee38ef84a18c5e29789729808
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 21:16:41 +0900

    refactor(Rank): Rank 클래스의 각 상수에 일치 개수 추가

--- src/main/java/lotto/constant/Rank.java
+++ src/main/java/lotto/constant/Rank.java
@@ -1,16 +1,18 @@
 package lotto.constant;
 
 public enum Rank {
-    FIRST_PRIZE(2_000_000_000),
-    SECOND_PRIZE(30_000_000),
-    THIRD_PRIZE(1_500_000),
-    FOURTH_PRIZE(50_000),
-    FIFTH_PRIZE(5_000),
-    LOSING(0);
+    FIRST_PRIZE(6,2_000_000_000),
+    SECOND_PRIZE(5,30_000_000),
+    THIRD_PRIZE(5,1_500_000),
+    FOURTH_PRIZE(4,50_000),
+    FIFTH_PRIZE(3,5_000),
+    LOSING(0,0);
 
+    private final int matchCount;
     private final long winningAmount;
 
-    Rank(long winningAmount) {
+    Rank(int matchCount,long winningAmount) {
+        this.matchCount = matchCount;
         this.winningAmount = winningAmount;
     }
 }

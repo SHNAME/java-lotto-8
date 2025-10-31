d5323376ff2c5c34e4fa521ca32f9e776446bb3d
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 19:32:39 +0900

    feat(LottoMachine): 로또 당첨 번호 관리 기능 구현
    - 사용자가 입력한 당첨 번호와 보너스 번호를 관리하는 LottoMachine 구현
    - 당첨 번호와 보너스 번호 사이에 중복이 있는지 검증

--- src/main/java/lotto/domain/LottoMachine.java
+++ src/main/java/lotto/domain/LottoMachine.java
@@ -7,7 +7,14 @@ public class LottoMachine {
     private final Integer BonusNumber;
 
     public LottoMachine(List<Integer> winningNumbers, Integer bonusNumber) {
+        validateNoDuplicateWithBonusNumber(winningNumbers, bonusNumber);
         this.winningNumbers = winningNumbers;
         this.BonusNumber = bonusNumber;
+    }
+
+    private void validateNoDuplicateWithBonusNumber(List<Integer> winningNumbers, Integer bonusNumber) {
+        if(winningNumbers.stream().anyMatch(lottoNumber -> lottoNumber.equals(bonusNumber))){
+            throw new IllegalArgumentException("당첨 번호와 보너스 번호 사이에 중복은 없어야 합니다.");
+        }
     }
 }

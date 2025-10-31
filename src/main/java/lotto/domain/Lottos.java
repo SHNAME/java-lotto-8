4f0de5be8dfccbf33190662554bd93809d93e21b
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 14:54:24 +0900

    feat(Lottos): 생성된 로또를 관리할 Lottos 클래스 생성

--- src/main/java/lotto/domain/Lottos.java
+++ src/main/java/lotto/domain/Lottos.java
@@ -7,9 +7,13 @@ import lotto.util.generator.LottoNumberGenerator;
 public class Lottos {
     private final List<Lotto> lottos = new ArrayList<>();
 
-    public Lottos(int count){
-        for(int i = 0; i<count;i++){
+    public Lottos(int count) {
+        for (int i = 0; i < count; i++) {
             lottos.add(new Lotto(LottoNumberGenerator.createLottoNumbers()));
         }
+    }
+
+    public int getLottoCount() {
+        return lottos.size();
     }
 }

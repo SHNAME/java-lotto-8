8cf5d1a943ef6f1d97ec347d49c4c511f852e8ca
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 10:37:23 +0900

    test: 입력된 당첨 번호를 파싱하여 List로 반환하는 테스트 코드 작성
    문자열에 입력된 각 숫자가 1과 45사이에 있는지 확인하기 위한 파싱 처리 테스트

--- src/test/java/lotto/ValidationTest.java
+++ src/test/java/lotto/ValidationTest.java
@@ -1,5 +1,7 @@
 package lotto;
 
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
@@ -7,7 +9,7 @@ public class ValidationTest {
 
 
     @Test
-    void 로또_가격에_숫자가_아닌_문자를_입력한_경우(){
+    void 로또_가격에_숫자가_아닌_문자를_입력한_경우() {
         String input1 = "-122";
         String input2 = "0191";
         String regex = "^[1-9][0-9]*$";
@@ -16,7 +18,7 @@ public class ValidationTest {
     }
 
     @Test
-    void 로또_가격을_정상적으로_입력한_경우(){
+    void 로또_가격을_정상적으로_입력한_경우() {
         String input1 = "1222";
         String input2 = "11";
         String regex = "^[1-9][0-9]*$";
@@ -25,7 +27,7 @@ public class ValidationTest {
     }
 
     @Test
-    void 로또_가격이_1000원으로_나눴을_때_나머지가_있는_경우(){
+    void 로또_가격이_1000원으로_나눴을_때_나머지가_있는_경우() {
         String input1 = "3250";
         String input2 = "333";
         String input3 = "1320";
@@ -35,7 +37,7 @@ public class ValidationTest {
     }
 
     @Test
-    void 로또_가격이_1000원으로_나눴을_때_나머지가_0_정상적인_경우(){
+    void 로또_가격이_1000원으로_나눴을_때_나머지가_0_정상적인_경우() {
         String input1 = "4000";
         String input2 = "30000";
         String input3 = "1000";
@@ -45,10 +47,10 @@ public class ValidationTest {
     }
 
     @Test
-    void 당첨_번호에_형식이_잘못된_경우(){
-        String regex ="^\\d+(,\\d+){5}$";
+    void 당첨_번호에_형식이_잘못된_경우() {
+        String regex = "^\\d+(,\\d+){5}$";
         String input1 = "1,2,3,4,5";
-        String input2 ="1,2,3,4,5,6,7";
+        String input2 = "1,2,3,4,5,6,7";
         String input3 = "1,2$3,4,5";
         String input4 = "1, 2, 3, 4, 5";
         Assertions.assertFalse(input1.matches(regex));
@@ -58,12 +60,24 @@ public class ValidationTest {
     }
 
     @Test
-    void 당첨_번호에_형식이_올바르게_입력한_경우(){
-        String regex ="^\\d+(,\\d+){5}$";
+    void 당첨_번호에_형식이_올바르게_입력한_경우() {
+        String regex = "^\\d+(,\\d+){5}$";
         String input1 = "1,2,3,4,5,6";
         String input2 = "33,44,25,32,3232,100000";
         Assertions.assertTrue(input1.matches(regex));
         Assertions.assertTrue(input2.matches(regex));
+    }
+
+    @Test
+    void 당첨_번호_파싱을_파싱하여_list로_반환한다() {
+        String input = "1,2,3,4,5,6";
+        List<Integer> list = Arrays.stream(input.split(","))
+                .map(Integer::parseInt)
+                .toList();
+        Assertions.assertEquals(1, list.get(0));
+        Assertions.assertEquals(2, list.get(1));
+        Assertions.assertEquals(3, list.get(2));
+        Assertions.assertEquals(4, list.get(3));
     }
 
 

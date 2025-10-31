b2f468c8909f03ec40989dcc09e05880abf70747
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 18:00:07 +0900

    test: 로또 당첨 번호 관리 기능 테스트
    로또 번호 당첨 번호 관리를 위해 LottoMachine 클래스가 필요함
    LottoMachine 클래스를 생성할 때 당첨번호와 보너스 번호를 넘겨줌

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,9 +1,15 @@
 package lotto;
 
+import java.util.List;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-public class LottoMachine {
+public class LottoMachineTest {
 
     @Test
-
+    void 로또_당첨_번호_관리_기능_테스트() {
+        List<Integer> winningNumber = List.of(1, 2, 3, 4, 5, 6);
+        Integer bonusNumber = 33;
+        LottoMachine lottoMachine = new LottoMachine(winningNumber,bonusNumber);
+    }
 }

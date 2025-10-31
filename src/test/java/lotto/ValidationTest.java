52659e777be8a08b66a178b32bcf8fe4b553d1d3
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 09:38:39 +0900

    test: 로또 구매 가격 입력 검증 테스트
    사용자가 형식에 맞지 않는 문자열을 입력한 경우,
    사용자가 형식에 맞게 문자열을 입력한 경우를 테스트

--- src/test/java/lotto/ValidationTest.java
+++ src/test/java/lotto/ValidationTest.java
@@ -1,4 +1,27 @@
 package lotto;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class ValidationTest {
+
+
+    @Test
+    void 로또_가격에_숫자가_아닌_문자를_입력한_경우(){
+        String input1 = "-122";
+        String input2 = "0191";
+        String regex = "^[1-9][0-9]*$";
+        Assertions.assertFalse(input1.matches(regex));
+        Assertions.assertFalse(input2.matches(regex));
+    }
+
+    @Test
+    void 로또_가격을_정상적으로_입력한_경우(){
+        String input1 = "1222";
+        String input2 = "11";
+        String regex = "^[1-9][0-9]*$";
+        Assertions.assertTrue(input1.matches(regex));
+        Assertions.assertTrue(input2.matches(regex));
+    }
+
 }

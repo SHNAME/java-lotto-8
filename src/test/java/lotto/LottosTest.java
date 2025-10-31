286d0bd70290ed61d6bb1cb7bcf85be4a6209fda
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 14:53:17 +0900

    test: 로또 생성 기능에 대한 테스트 코드 작성
    - 생성된 로또를 관리하기 위해 Lottos 클래스 생성
    - 생성자를 통해 구매한 로또 개수를 입력 받아 로또를 생성

--- src/test/java/lotto/LottosTest.java
+++ src/test/java/lotto/LottosTest.java
@@ -1,6 +1,7 @@
 package lotto;
 
 import lotto.domain.Lottos;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class LottosTest {
@@ -8,6 +9,6 @@ public class LottosTest {
     void 사용자가_구매한_로또_개수만큼_로또를_생성한다() {
         Integer count = 5;
         Lottos lottos = new Lottos(5);
-
+        Assertions.assertThat(lottos.getLottoCount()).isEqualTo(count);
     }
 }

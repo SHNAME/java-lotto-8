e6bd234d95f128a6eb88b0d58f20fc3f44eb82b7
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 09:58:59 +0900

    feat(InputValidator): 로또 구매 가격 입력 검증 기능 구현
    사용자가 입력한 로또 가격에 대한 검증을 처리한다.

--- src/main/java/lotto/validator/InputValidator.java
+++ src/main/java/lotto/validator/InputValidator.java
@@ -3,15 +3,20 @@ package lotto.validator;
 public final class InputValidator {
     private static final String PURCHASE_PRICE = "^[1-9][0-9]*$";
 
+    public static void validatePurchasePrice(String userInput) {
+        validatePriceFormat(userInput);
+        validateAmountUnit(userInput);
+    }
+
     private static void validatePriceFormat(String userInput) {
-        if(!userInput.matches(PURCHASE_PRICE)){
-            throw  new IllegalArgumentException("구매 가격을 잘못 입력하셨습니다.");
+        if (!userInput.matches(PURCHASE_PRICE)) {
+            throw new IllegalArgumentException("구매 가격을 잘못 입력하셨습니다.");
         }
     }
 
-    private static void validateAmountUnit(String userInput){
-        if((Integer.parseInt(userInput) % 1000) !=0){
-            throw  new IllegalArgumentException("1000원 단위로 입력해주세요");
+    private static void validateAmountUnit(String userInput) {
+        if ((Integer.parseInt(userInput) % 1000) != 0) {
+            throw new IllegalArgumentException("1000원 단위로 입력해주세요");
         }
     }
 

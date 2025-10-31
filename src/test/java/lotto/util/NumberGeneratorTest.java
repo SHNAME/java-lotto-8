cbd00fe04565424c0413b817b96b80aa5eebe90b
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 12:47:58 +0900

    test: 로또 번호 생성 기능 테스트
    - 1부터 45까지 무작위 수 6개를 중복 없이 추출하는 기능에 대한 테스트 코드 작성

--- src/test/java/lotto/util/NumberGeneratorTest.java
+++ src/test/java/lotto/util/NumberGeneratorTest.java
@@ -1,4 +1,19 @@
 package lotto.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class NumberGeneratorTest {
+
+    @Test
+    void 중복되지_않은_숫자를_생성하는_테스트() {
+        List<Integer> numbers = Randoms.pickUniqueNumbersInRange(1, 45, 6);
+        Set<Integer> uniqueNumbers = new HashSet<>(numbers);
+        Assertions.assertThat(numbers.size()).isEqualTo(6);
+        Assertions.assertThat(uniqueNumbers.size()).isEqualTo(numbers.size());
+    }
 }

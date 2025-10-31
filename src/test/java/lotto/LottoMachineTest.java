6ba6e5b3e9fbbd14840c8e2daf1e9cae63c72f44
White_DeepDive <tlgud119@yu.ac.kr>
2025-10-31 20:35:17 +0900

    test: 내가 구매한 로또 번호와 당첨 번호와 비교하는 테스트 코드 작성
    - 내가 구매한 로또 번호와 당첨 번호를 교집합을 통해 일치하는 개수를 파악한다.
    - 만약 일치하는 개수가 5개라면 보너스 번호도 비교한다.
    - 위 상황에서 보너스 번호가 일치한다면 2등 당첨으로 처리

--- src/test/java/lotto/LottoMachineTest.java
+++ src/test/java/lotto/LottoMachineTest.java
@@ -1,5 +1,8 @@
 package lotto;
 
+import static org.assertj.core.api.Assertions.*;
+
+import java.util.ArrayList;
 import java.util.List;
 import lotto.domain.LottoMachine;
 import org.junit.jupiter.api.Assertions;
@@ -17,6 +20,7 @@ public class LottoMachineTest {
         boolean result = winningNumber.stream().anyMatch(num -> num.equals(bonusNumber));
         Assertions.assertTrue(result);
     }
+
     @DisplayName("로또 번호와 보너스 번호 사이에 중복이 없는 경우")
     @Test
     void 로또_당첨_번호_관리_기능_테스트() {
@@ -25,6 +29,37 @@ public class LottoMachineTest {
         LottoMachine lottoMachine = new LottoMachine(winningNumber, bonusNumber);
         boolean result = winningNumber.stream().anyMatch(num -> num.equals(bonusNumber));
         Assertions.assertFalse(result);
+    }
+
+    @Test
+    void 구매한_로또_번호와_당첨_번호가_5등인_경우() {
+        List<Integer> lottoNumbers = List.of(1, 3, 5, 14, 22, 45);
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> intersectionNumbers = new ArrayList<>(lottoNumbers);
+        intersectionNumbers.retainAll(winningNumbers);
+        assertThat(intersectionNumbers.size()).isEqualTo(3);
+    }
+
+    @Test
+    void 구매한_로또_번호와_당첨_번호가_꽝인_경우()  {
+        List<Integer> lottoNumbers = List.of(13, 14, 16, 38, 42, 45);
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        List<Integer> intersectionNumbers = new ArrayList<>(lottoNumbers);
+        intersectionNumbers.retainAll(winningNumbers);
+        assertThat(intersectionNumbers.size()).isEqualTo(0);
+    }
+
+    @DisplayName("5개가 일치하는 경우 보너스 번호도 비교한다.")
+    @Test
+    void 구매한_로또_번호와_당첨_번호가_2등_경우()  {
+        List<Integer> lottoNumbers = List.of(1, 1, 3, 4, 5, 45);
+        List<Integer> winningNumbers = List.of(1, 2, 3, 4, 5, 6);
+        Integer bonusNumber = 45;
+        List<Integer> intersectionNumbers = new ArrayList<>(lottoNumbers);
+        intersectionNumbers.retainAll(winningNumbers);
+        boolean result = lottoNumbers.contains(bonusNumber);
+        assertThat(intersectionNumbers.size()).isEqualTo(5);
+        Assertions.assertTrue(result);
     }
 
 

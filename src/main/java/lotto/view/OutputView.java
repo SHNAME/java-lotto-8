f3af2cbca0a2965120e804164044d0582307bdc6
White_DeepDive <tlgud119@yu.ac.kr>
2025-11-01 11:45:08 +0900

    feat(OutputView): 당첨 통계 출력 기능 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,15 +1,26 @@
 package lotto.view;
 
+import java.text.DecimalFormat;
 import java.util.List;
+import lotto.constant.Rank;
 import lotto.domain.Lotto;
 
 public final class OutputView {
 
-    public static void printPurchaseLottoNumbers(List<Lotto> lottos){
+    public static void printPurchaseLottoNumbers(List<Lotto> lottos) {
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }
         System.out.println();
+    }
+
+    public static void printWinningStatistics(List<String> ranks, double rateOfReturn) {
+        System.out.println("당첨 통계\n");
+        for (String rank : ranks) {
+            System.out.println(rank);
+        }
+        DecimalFormat df = new DecimalFormat("#,###.#");
+        System.out.println("총 수익률은 " + df.format(rateOfReturn) + "%입니다.");
     }
 
 }

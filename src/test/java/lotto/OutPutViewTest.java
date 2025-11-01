9f98db6a89a90d8c95b2522420c14a9a4154fc5e
White_DeepDive <tlgud119@yu.ac.kr>
2025-11-01 11:32:44 +0900

    test:당첨 통계를 출력하는 테스트 코드 작성
    - 당첨 통계 출력이 정상적으로 작동하는지 확인하기 위한 테스트 코드 작성

--- src/test/java/lotto/OutPutViewTest.java
+++ src/test/java/lotto/OutPutViewTest.java
@@ -1,9 +1,10 @@
 package lotto;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
-import lotto.domain.Lotto;
-import org.junit.jupiter.api.Assertions;
+import lotto.constant.Rank;
 import org.junit.jupiter.api.Test;
 
 public class OutPutViewTest {
@@ -12,5 +13,16 @@ public class OutPutViewTest {
     public void 구매한_로또_번호들을_출력하는_테스트() {
         List<Integer> lotto1 = List.of(1, 2, 3, 4, 5, 6);
         System.out.println(lotto1);
+    }
+
+    @Test
+    public void 당첨_통계를_출력하는_테스트(){
+        List<Rank> result = List.of(Rank.LOSING,Rank.FIRST_PRIZE,Rank.SECOND_PRIZE);
+        List<String> list = Arrays.stream(Rank.values())
+                .sorted(Comparator.comparingLong(r -> r.getWinningAmount()))
+                .filter(rank -> !rank.equals(Rank.LOSING))
+                .map(rank ->
+                        rank.getResultString()+" - " + Collections.frequency(result,rank) + "개"
+                ).toList();
     }
 }

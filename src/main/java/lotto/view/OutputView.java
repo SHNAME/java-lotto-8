be1b74e3cfa37035532747502a2b753f56c37ef0
White_DeepDive <tlgud119@yu.ac.kr>
2025-11-01 11:57:35 +0900

    fix(OutputView): 로또 번호 출력 기능을 요구사항에 맞게 변경

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -2,12 +2,12 @@ package lotto.view;
 
 import java.text.DecimalFormat;
 import java.util.List;
-import lotto.constant.Rank;
 import lotto.domain.Lotto;
 
 public final class OutputView {
 
     public static void printPurchaseLottoNumbers(List<Lotto> lottos) {
+        System.out.println(lottos.size()+"개를 구매했습니다.");
         for (Lotto lotto : lottos) {
             System.out.println(lotto);
         }

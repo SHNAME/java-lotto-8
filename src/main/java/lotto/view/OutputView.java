cd4ec4bd8901889bfddcf6318979c2fd82ba4c1c
White_DeepDive <tlgud119@yu.ac.kr>
2025-11-01 10:32:06 +0900

    feat(OutputView): 구매한 로또 번호 출력 기능 구현
    - 구매한 로또 개수 만큼 로또 번호를 출력하는 함수 구현

--- src/main/java/lotto/view/OutputView.java
+++ src/main/java/lotto/view/OutputView.java
@@ -1,6 +1,14 @@
 package lotto.view;
 
+import java.util.List;
+import lotto.domain.Lotto;
+
 public final class OutputView {
 
+    public static void printPurchaseLottoNumbers(List<Lotto> lottos){
+        for (Lotto lotto : lottos) {
+            System.out.println(lotto);
+        }
+    }
 
 }

4d0a015964ef5c211e0b1f55e92e8e07690ec65f
White_DeepDive <tlgud119@yu.ac.kr>
2025-11-02 21:12:26 +0900

    refactor: 예외 문자열 상수화를 위한 공통 상수 클래스 추가
    - 각 코드에서 예외 메세지를 개별적으로 작성하고 잇어 유지보수가 어려웠음
    - 예외 메세지를 상수로ㅗ 관리하는 클래스 추가로 일관성과 재사용성 개선

---
+++ src/main/java/lotto/constant/ExceptionMessage.java
@@ -0,0 +1,20 @@
+package lotto.constant;
+
+public enum ExceptionMessage {
+    WINNING_NUMBERS_DUPLICATION("당첨 번호는 중복 입력이 불가능합니다."),
+    INVALID_NUMBER_OF_LOTTO_NUMBERS("로또 번호는 반드시 6개를 입력해야 합니다."),
+    BETWEEN_WINNING_NUMBERS_AND_BONUS_NUMBER("당첨 번호와 보너스 번호는 서로 중복될 수 없습니다"),
+    PRICE_FORMAT_INVALID("구매 금액 형식이 올바르지 않습니다. 숫자만 입력해주세요."),
+    PURCHASE_UNIT_INVALID("구매 금액은 1000원 단위로 입력해야 합니다."),
+    WINNING_NUMBERS_FORMAT_INVALID("당첨 번호 입력 형식이 올바르지 않습니다. 숫자를 쉼표(,)로 구분해주세요. 예: 1,2,3,4,5,6"),
+    WINNING_NUMBERS_RANGE_INVALID("당첨 번호는 1부터 45 사이의 값만 가능합니다."),
+    BONUS_NUMBER_FORMAT_INVALID("보너스 번호 형식이 올바르지 않습니다. 숫자만 입력해주세요."),
+    BONUS_NUMBER_RANGE_INVALID("보너스 번호는 1부터 45 가이의 값만 가능합니다.");
+
+
+    private final String message;
+
+    ExceptionMessage(String message){
+        this.message = message;
+    }
+}
